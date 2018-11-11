36668f5499f1eaec150629a2cf16868262526576
Benjamin Wood <benjaminwood@gmail.com>
2018-11-10 22:49:10 -0800

    Add request spec for after_inactive_sign_up_path_for - #1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,13 +3,14 @@ require 'simplecov'
 
 SimpleCov.start 'rails'
 
-require 'support/factory_bot'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
+require 'support/devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
---
+++ spec/requests/user_registration_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "User registration", :type => :request do
+  describe '#create' do
+    it 'redirect to new user session path after signup' do
+      post user_registration_path, params: { user: { email: 'foo@example.com', password: 'foobar', password_confirmation: 'foobar' } }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
