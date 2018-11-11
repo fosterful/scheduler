03455e6a72018e881a1b835c13487827056176a1
Benjamin Wood <benjaminwood@gmail.com>
2018-11-10 22:01:46 -0800

    Configure webpacker/add yarn packages

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+import Foundation from '../src/foundation_loader';
+import '../src/application.scss';
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+
+Rails.start();
+Turbolinks.start();
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,9 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 const environment = require('./environment')
 
-const devConfig = environment.toWebpackConfig();
-devConfig.devServer.watchOptions = {
-  ignored: /node_modules/,
-  aggregateTimeout: 300,
-  poll: 500,
-};
-module.exports = devConfig;
+module.exports = environment.toWebpackConfig()
