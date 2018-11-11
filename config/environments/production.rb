d3fb2b1cb32cea05215637392c057c9c8bb342af
Benjamin Wood <benjaminwood@gmail.com>
2018-11-10 22:18:57 -0800

    Add default_url_options to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'omd-scheduler.herokuapp.com' }
+
    # Setup the mailer config
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
