c912ea353ea5fe7ffdf76348a96dbbd5bc745cc6
Benjamin Wood <benjaminwood@gmail.com>
2018-12-09 16:14:13 -0800

    Only init smartystreets if credentials are present

--- config/initializers/smartystreets.rb
+++ config/initializers/smartystreets.rb
@@ -1,18 +1,20 @@
 credentials = Rails.application.credentials.smartystreets
 
-Geocoder.config[:smarty_streets] ||= {
-  api_key: [
-    credentials[:auth_id],
-    credentials[:auth_token]
-  ]
-}
+if credentials.present?
+  Geocoder.config[:smarty_streets] ||= {
+    api_key: [
+      credentials[:auth_id],
+      credentials[:auth_token]
+    ]
+  }
 
-# This looks for auth_id as an env var
-# by default. We don't have one :-)
-module MainStreet
-  class AddressVerifier
-    def lookup
-      :smarty_streets
+  # This looks for auth_id as an env var
+  # by default. We don't have one :-)
+  module MainStreet
+    class AddressVerifier
+      def lookup
+        :smarty_streets
+      end
     end
   end
 end
