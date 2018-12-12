1128072907c4219723c5d56ec0c91f5f19b2f0e9
Benjamin Wood <benjaminwood@gmail.com>
2018-12-11 22:56:08 -0800

    Add offices to user dashboard/show view - #1

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -45,17 +45,18 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :invited_by,
-    :id,
-    :email
+    :email,
+    :id
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :invited_by,
-    :id,
     :email,
+    :role,
+    :invited_by,
+    :offices,
+    :id,
     :unconfirmed_email,
     :created_at,
     :updated_at,
@@ -63,7 +64,6 @@ class UserDashboard < Administrate::BaseDashboard
     :locked_at,
     :sign_in_count,
     :current_sign_in_at,
-    :role
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -78,7 +78,7 @@ class UserDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how users are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(user)
-  #   "User ##{user.id}"
-  # end
+  def display_resource(user)
+    user.email
+  end
 end
