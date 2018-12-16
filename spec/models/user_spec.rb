52154b46fe1946df561338c959ebf9e89ac0ef7c
Benjamin Wood <benjaminwood@gmail.com>
2018-12-15 19:12:33 -0800

    Add spec for User#has_at_least_one_office

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe User, type: :model do
       expect(user.volunteer?).to eq(true)
     end
   end
+
+  describe '#has_at_least_one_office' do
+    it 'adds an error if no office is associated' do
+      user.offices.clear
+      expect(user.valid?).to be(false)
+      expect(user.errors.full_messages.first).to eq('At least one office assignment is required')
+    end
+  end
 end
