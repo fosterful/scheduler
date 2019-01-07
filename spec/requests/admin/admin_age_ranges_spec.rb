b0b656f228d0d1dc7f8e2f53036dad043327e994
Daniel P. Clark <6ftdan@gmail.com>
2019-01-07 17:52:24 -0500

    Spec for AgeRange - #9

---
+++ spec/requests/admin/admin_age_ranges_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Admin age_ranges spec", type: :request do
+  before { sign_in create :user }
+
+  it 'does not allow unauthorized access' do
+    get admin_age_ranges_path
+    expect(response).to redirect_to(:root)
+  end
+
+  describe "#create" do
+    before { sign_in create :user, role: 'admin' }
+
+    it "redirects to show view after creating the office" do
+      post admin_age_ranges_path, params: { age_range: { min: 1, max: 1 } }
+      expect(response).to redirect_to(admin_age_range_path(AgeRange.last))
+    end
+
+    it 'renders errors if present' do
+      post admin_age_ranges_path, params: { age_range: { min: 1 } }
+      expect(response.body).to include('error')
+    end
+  end
+end
