74528828c25958e59351399c3639441baed1dc81
Benjamin Wood <benjaminwood@gmail.com>
2019-01-08 10:31:48 -0800

    Remove non-required attributes from factory - #7

--- spec/factories/block_outs.rb
+++ spec/factories/block_outs.rb
@@ -2,8 +2,5 @@ FactoryBot.define do
   factory :block_out do
     association :user, strategy: :build
     start_at { "2019-01-08 16:33:26" }
-    end_at { "2019-01-08 16:33:26" }
-    rrule { nil }
-    reason { "On vacation" }
   end
 end
