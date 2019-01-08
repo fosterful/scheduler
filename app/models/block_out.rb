3c0818b027368840723ed82c7fba3a41973d9895
Benjamin Wood <benjaminwood@gmail.com>
2019-01-08 12:28:20 -0800

    Validate start_at - #7

--- app/models/block_out.rb
+++ app/models/block_out.rb
@@ -1,3 +1,4 @@
 class BlockOut < ApplicationRecord
   belongs_to :user
+  validates :start_at, presence: true
 end
--- spec/factories/block_outs.rb
+++ spec/factories/block_outs.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :block_out do
     association :user, strategy: :build
-    start_at { "2019-01-08 16:33:26" }
+    start_at { 1.day.from_now }
   end
 end
