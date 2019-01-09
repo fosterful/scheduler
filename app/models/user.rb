3843f18ae7b3844f0cd5be3038b654d58190aa61
Benjamin Wood <benjaminwood@gmail.com>
2019-01-09 14:48:03 -0800

    Always require first & last name - #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,21 +23,22 @@ class User < ApplicationRecord
   has_and_belongs_to_many :offices
   has_many :block_outs
 
-  validates :first_name, :last_name, :birth_date,
-            :phone, :resident_since, :discovered_omd_by,
-            presence: true, if: :require_profile_attributes?
+  validates :first_name, :last_name, presence: true, if: :invitation_accepted_at?
+
+  validates :birth_date, :phone, :resident_since, :discovered_omd_by,
+            presence: true, if: :require_volunteer_profile_attributes?
 
   validates :medical_limitations, :conviction,
             inclusion: { in: [true, false], message: "can't be blank" },
-            if: :require_profile_attributes?
+            if: :require_volunteer_profile_attributes?
 
   validates :medical_limitations_desc,
             presence: true,
-            if: -> { require_profile_attributes? && medical_limitations? }
+            if: -> { require_volunteer_profile_attributes? && medical_limitations? }
 
   validates :conviction_desc,
             presence: true,
-            if: -> { require_profile_attributes? && conviction? }
+            if: -> { require_volunteer_profile_attributes? && conviction? }
 
   validates :role, inclusion: { in: ROLES, message: '%{value} is not a valid role' }
   validates :time_zone, presence: true, if: :invitation_accepted_at?
@@ -60,7 +61,7 @@ class User < ApplicationRecord
 
   private
 
-  def require_profile_attributes?
+  def require_volunteer_profile_attributes?
     (volunteer? || coordinator?) && invitation_accepted_at?
   end
 end
