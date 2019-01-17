eb5a7c19d32520243cf95171b15afa73e5c9696d
Benjamin Wood <benjaminwood@gmail.com>
2019-01-17 06:50:26 -0800

    Fix timezone setting bug - #7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  around_action :set_time_zone
+  around_action :set_time_zone, if: :user_signed_in?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
