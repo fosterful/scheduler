467afe9cec725b59da86fdad167457765647f6dc
Nate Shoemaker <nate@hint.io>
2019-01-18 08:49:59 -0800

    <AddBlackoutButton /> - change to functional component from class, no need to bind this - #7

--- app/javascript/components/blockouts/AddBlockoutButton/index.js
+++ app/javascript/components/blockouts/AddBlockoutButton/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import SchedulerContext from 'blockouts/contexts/scheduler'
 
-class AddBlockoutButton extends React.Component {
-  render () {
-    return (
-      <SchedulerContext.Consumer>
-        {({ setModalInfo }) => (
-          <button
-            type='button'
-            className='primary button tiny add-new-blockout-button'
-            onClick={setModalInfo.bind(this, { component: 'NewBlockoutModal' })}
-          >
-            Add Blockout
-          </button>
-        )}
-      </SchedulerContext.Consumer>
-    )
-  }
-}
+const AddBlockoutButton = () =>
+  <SchedulerContext.Consumer>
+    {({ setModalInfo }) => (
+      <button
+        type='button'
+        className='primary button tiny add-new-blockout-button'
+        onClick={setModalInfo.bind(null, { component: 'NewBlockoutModal' })}
+      >
+        Add Blockout
+      </button>
+    )}
+  </SchedulerContext.Consumer>
+
 export default AddBlockoutButton
