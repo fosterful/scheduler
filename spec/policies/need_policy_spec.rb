e2b461c23293f19c1d4af4a131a7566e2eac7903
Kyle Boe <ksboe1@gmail.com>
2019-01-22 13:57:44 -0800

    add scope specs to need policy spec

--- spec/policies/need_policy_spec.rb
+++ spec/policies/need_policy_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe NeedPolicy do
   let(:creator) { build :user, role: 'social_worker' }
-  let(:record) { build :need, user: creator }
+  let(:record) { create :need, user: creator }
 
   subject { described_class.new(user, record) }
 
   context 'for volunteers' do
-    let(:user) { build :user, role: 'volunteer' }
+    let(:user) { create :user, role: 'volunteer' }
 
     context 'of the office' do
       before { record.office.users << user }
@@ -19,16 +19,28 @@ RSpec.describe NeedPolicy do
       it { is_expected.to forbid_action(:edit) }
       it { is_expected.to forbid_action(:update) }
       it { is_expected.to forbid_action(:destroy) }
+
+      describe '.scope' do
+        it 'scopes the query' do
+          expect(described_class::Scope.new(user.reload, Need.all).resolve).to include(record)
+        end
+      end
     end
 
     context 'not of the office' do
       it { is_expected.to permit_action(:index) }
       it { is_expected.to forbid_action(:show) }
+
+      describe '.scope' do
+        it 'scopes the query' do
+          expect(described_class::Scope.new(user.reload, Need.all).resolve).not_to include(record)
+        end
+      end
     end
   end
 
   context 'for social workers' do
-    let(:user) { build :user, role: 'social_worker' }
+    let(:user) { create :user, role: 'social_worker' }
     before { record.office.users << user }
 
     it { is_expected.to permit_action(:index) }
@@ -38,5 +50,29 @@ RSpec.describe NeedPolicy do
     it { is_expected.to permit_action(:edit) }
     it { is_expected.to permit_action(:update) }
     it { is_expected.to permit_action(:destroy) }
+
+    describe '.scope' do
+      it 'scopes the query' do
+        expect(described_class::Scope.new(user.reload, Need.all).resolve).to include(record)
+      end
+    end
+  end
+
+  context 'for admins' do
+    let(:user) { create(:user, role: 'admin') }
+
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:new) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+
+    describe '.scope' do
+      it 'scopes the query' do
+        expect(described_class::Scope.new(user.reload, Need.all).resolve).to match_array Need.all
+      end
+    end
   end
 end
