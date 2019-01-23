a1d54586aa28731c1d207296da501e4a5823bad1
Kyle Boe <ksboe1@gmail.com>
2019-01-23 15:54:00 -0800

    start_at datepicker things

--- app/javascript/components/forms/DayPicker/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import DayPicker from 'react-day-picker'
-import 'react-day-picker/lib/style.css'
-
-const Foobar = ({ blockouts }) =>
-  <DayPicker />
-
-export default Foobar
