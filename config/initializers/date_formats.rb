8e8f2731ec7f83574043e5d8bff8898bae92bef9
Kyle Boe <ksboe1@gmail.com>
2019-01-23 16:14:05 -0800

    revert config dir

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,1 @@
-# frozen_string_literal: true
-
 Time::DATE_FORMATS[:short_with_time] = ->(date) { date.strftime('%m/%d/%Y at %l:%M%p').squeeze(' ') }
--- config/initializers/smartystreets.rb
+++ config/initializers/smartystreets.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 credentials = Rails.application.credentials.smartystreets || {}
 
 Geocoder.config[:smarty_streets] ||= {
