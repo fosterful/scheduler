4f28be039af7925416b75dc3bee082566fa4b164
Kyle Boe <ksboe1@gmail.com>
2019-01-28 14:54:30 -0800

    extend needs factory

--- spec/factories/needs.rb
+++ spec/factories/needs.rb
@@ -7,5 +7,15 @@ FactoryBot.define do
     start_at { Time.zone.now }
     expected_duration { 120 }
     number_of_children { 1 }
+
+    factory :need_with_shifts do
+      transient do
+        shifts_count { 2 }
+      end
+
+      after(:create) do |need, evaluator|
+        create_list(:shift, evaluator.shifts_count, need: need)
+      end
+    end
   end
 end
