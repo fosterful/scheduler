d6c7573e4569b34de2cf4f79dc59e57c2d845033
Kyle Boe <ksboe1@gmail.com>
2019-01-28 14:54:57 -0800

    add specs for updating shifts

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :shift do
     association :need, strategy: :build
-    association :user, strategy: :build
     start_at { Time.zone.now }
     duration { 60 }
   end
--- spec/requests/shifts_spec.rb
+++ spec/requests/shifts_spec.rb
@@ -2,10 +2,13 @@
 
 require "rails_helper"
 
-RSpec.describe "Needs", type: :request do
-  let(:user) { create :user }
-  before { sign_in user }
-
+RSpec.describe "Shifts", type: :request do
+  let(:office) { create :office }
+  let(:user) { create :user, role: 'admin', offices: [office] }
+  let(:volunteer) { create :user, role: 'volunteer', offices: [office] }
+  let(:need) { create :need_with_shifts, user: user, office: office }
+  let!(:shift) { need.shifts.first }
+  before { sign_in volunteer }
   describe '#new' do
   end
 
@@ -13,6 +16,20 @@ RSpec.describe "Needs", type: :request do
   end
 
   describe '#update' do
+    context 'success' do
+      it 'sets the flash to display the successful change message' do
+        expect(Services::SendShiftStatusNotifications).to receive(:call).and_return(true)
+        put need_shift_path(need, shift), params: { shift: { user_id: volunteer.id } }
+        expect(flash[:notice]).to eql('Shift Claimed!')
+      end
+    end
+    context 'failure' do
+      it 'sets the flash to display an error message' do
+        expect_any_instance_of(Shift).to receive(:save).and_return(false)
+        put need_shift_path(need, shift), params: { shift: { user_id: volunteer.id } }
+        expect(flash[:alert]).to eql('Whoops! something went wrong.')
+      end
+    end
   end
 
   describe '#destroy' do
