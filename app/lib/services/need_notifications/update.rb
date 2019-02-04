fb3cdc453d0f45c2fc2014d283c16a582bbbde3e
Kyle Boe <ksboe1@gmail.com>
2019-02-04 15:41:08 -0800

    properly scope users to notify

--- app/lib/services/need_notifications/update.rb
+++ app/lib/services/need_notifications/update.rb
@@ -7,12 +7,37 @@ module Services
       include Concord.new(:need, :url)
       include Adamantium::Flat
 
-      delegate :preferred_language, :notified_user_ids, :age_range_ids, to: :need
+      delegate :office, :preferred_language, :notified_user_ids,
+               :age_range_ids, :shifts to: :need
 
       def call
-        User.find(notified_user_ids).each do |user|
-          SendTextMessageWorker.perform_async(user.phone, "A Need at your office has been updated. #{url}" )
+        users_to_notify.each do |user|
+          SendTextMessageWorker.perform_async(user.phone, "A new Need has opened up at your local office! #{url}" )
         end
+      end
+
+      private
+
+      def users_to_notify
+        office
+          .users
+          .volunteerable
+          .where.not(id: notified_user_ids.push(user_id))
+          .available_within(shifts.first.start_at, shifts.last.end_at)
+          .then { |users| scope_users_by_language(users) }
+          .then { |users| scope_users_by_age_ranges(users) }
+      end
+
+      def scope_users_by_language(users)
+        return users unless preferred_language.present?
+
+        users.speaks_language(preferred_language)
+      end
+
+      def scope_users_by_age_ranges(users)
+        return users unless age_range_ids.any?
+
+        users.joins(:age_ranges).where(age_ranges: { id: age_range_ids })
       end
     end
   end
