91a8e83ef46d969fe87a864d1745cca8dd8a9ec6
Kyle Boe <ksboe1@gmail.com>
2019-02-14 15:35:28 -0800

    update addresses factory with faker and traits - #27

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -2,12 +2,28 @@
 
 FactoryBot.define do
   factory :address do
-    street { "1901 Main St" }
+    street { Faker::Address.street_address }
     street2 { nil }
-    city { "Vancouver" }
-    county { "Clark"}
-    state { "WA" }
-    postal_code { "98660" }
+    city { Faker::Address.city }
+    county { Faker::Address.street_name }
+    state { Faker::Address.state_abbr }
+    postal_code { Faker::Address.zip }
+
+    trait :wa do
+      street { "1901 Main St" }
+      city { "Vancouver" }
+      county { "Clark"}
+      state { "WA" }
+      postal_code { "98660" }
+    end
+
+    trait :or do
+      street { Faker::Address.street_address }
+      city { "Portland" }
+      county { "Multnoma" }
+      state { "OR" }
+      postal_code { "97035" }
+    end
 
     latitude { nil }
     longitude { nil }
