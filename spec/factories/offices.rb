5ea8d48516250eac41a74f754bf3330234fbd9df
Kyle Boe <ksboe1@gmail.com>
2019-02-14 17:36:23 -0800

    update offices factory to create cyclical relationship with address on create - #27

--- spec/factories/offices.rb
+++ spec/factories/offices.rb
@@ -4,8 +4,13 @@ FactoryBot.define do
   factory :office do
     name { "Vancouver Office" }
     region { 1 }
+
     after :build do |office|
-      office.address = build(:address, addressable: office)
+      office.address ||= build(:address, addressable: office)
+    end
+
+    after :create do |office|
+      office.address.addressable = office unless office.address.addressable.present?
     end
   end
 end
