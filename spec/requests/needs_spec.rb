3506c663ecdf823985dc627f62ad60b0055c925b
Benjamin Wood <benjaminwood@gmail.com>
2019-02-26 21:44:57 -0800

    Be more explicit in specs where possible

--- spec/requests/needs_spec.rb
+++ spec/requests/needs_spec.rb
@@ -36,8 +36,8 @@ RSpec.describe "Needs", type: :request do
   describe '#create' do
     context 'success' do
       it 'is redirects to the need' do
-        expect(Services::BuildNeedShifts).to receive(:call).and_return([])
-        expect(Services::NeedNotifications::Create).to receive(:call).and_return(true)
+        expect(Services::BuildNeedShifts).to receive(:call).and_return([]).once
+        expect(Services::NeedNotifications::Create).to receive(:call).and_return(true).once
         post needs_path, params: { need: attributes_for(:need).merge(office_id: need.office_id) }
         expect(response).to redirect_to(assigns(:need))
       end
@@ -62,7 +62,7 @@ RSpec.describe "Needs", type: :request do
   describe '#update' do
     context 'success' do
       it 'redirects to the need' do
-        expect(Services::NeedNotifications::Update).to receive(:call).and_return(true)
+        expect(Services::NeedNotifications::Update).to receive(:call).and_return(true).once
         put need_path(need), params: { need: { number_of_children: 20 } }
         expect(response).to redirect_to(assigns(:need))
       end
@@ -80,7 +80,7 @@ RSpec.describe "Needs", type: :request do
   describe '#destroy' do
     context 'success' do
       it 'redirects to the index view with success message' do
-        expect(Services::NeedNotifications::Destroy).to receive(:call).and_return(true)
+        expect(Services::NeedNotifications::Destroy).to receive(:call).and_return(true).once
         delete need_path(need)
         expect(response).to redirect_to(needs_path)
         expect(flash[:success]).to eql('Need successfully deleted')
