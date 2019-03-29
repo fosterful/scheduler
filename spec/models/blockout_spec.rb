3ab553b7b627ca2c3ad63ebd146899ac3abbdda0
Jason Dinsmore <jason@dinjas.com>
2019-03-29 09:26:57 -0700

    Repair spec

--- spec/models/blockout_spec.rb
+++ spec/models/blockout_spec.rb
@@ -95,7 +95,7 @@ RSpec.describe Blockout, type: :model do
     it 'duration_in_seconds' do
       result = blockout.duration_in_seconds
 
-      expect(result).to be(3600)
+      expect(result.round).to be(3600)
     end
   end
 
