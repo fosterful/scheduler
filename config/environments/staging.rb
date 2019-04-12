5e9a9b6e625d42ede74eccba303f80778e1b21c3
Kyle Boe <ksboe1@gmail.com>
2019-04-12 11:36:33 -0700

    WIP update staging to rely on env vars and production to use rails credentials

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
    config.action_mailer.smtp_settings = {
-     :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
-     :password => Rails.application.credentials.dig(:sendgrid, :password),
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
      :domain => 'officemomsanddads.com',
      :address => 'smtp.sendgrid.net',
      :port => 587,
