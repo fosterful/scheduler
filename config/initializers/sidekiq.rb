c3e53222b72efd389f3ea1ce3b76c6ea31f1e1a1
Kyle Boe <ksboe1@gmail.com>
2019-04-12 14:17:21 -0700

    update redis url building and config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # # frozen_string_literal: true
-credentials = Rails.application.credentials.redis || { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], db_num: ENV['REDIS_DB_NUM'] }
+credentials = Rails.application.credentials.redis || { username: ENV['REDIS_USERNAME'], password: ENV['REDIS_PASSWORD'], host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], db_num: ENV['REDIS_DB_NUM'] }
 
-def build_redis_url(redis_config)
-  "redis://#{redis_config[:host]}:#{redis_config[:port]}/#{redis_config[:db_num]}"
+def build_redis_url(config)
+  "redis://#{config[:username]}:#{config[:password]}@#{config[:host]}:#{config[:port]}/#{config[:db_num]}"
 end
 
 Sidekiq.configure_client do |config|
