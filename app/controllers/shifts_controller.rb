5cce2c1f9f636abb194b37138578b170ef0a4e2b
Mike Calhoun <mike@hint.io>
2019-04-17 09:44:53 -0700

    Display correct shift cliamed/release alert

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -28,7 +28,7 @@ class ShiftsController < ApplicationController
     @shift.assign_attributes(permitted_attributes(@shift))
     if @shift.save
       Services::ShiftNotifications::Update.call(@shift, current_user, user_id_was)
-      flash[:notice] = 'Shift Claimed!'
+      flash[:notice] = permitted_attributes(@shift).fetch("user_id").present? ? 'Shift Claimed!' : 'Shift Released!'
     else
       flash[:alert] = 'Whoops! something went wrong.'
     end
