79c243d0ea3699ba34ab7ed53d9e1729bb7cff82
Kyle Boe <ksboe1@gmail.com>
2019-05-07 14:58:19 -0700

    WIP office scopes - #27

--- app/models/office.rb
+++ app/models/office.rb
@@ -11,10 +11,10 @@ class Office < ApplicationRecord
 
   scope :volunteer_minutes, -> { joins(needs: :shifts).group('offices.id').where.not(shifts: { user_id: nil }).sum('shifts.duration') }
   scope :volunteer_minutes_by_state, -> { joins(:address, needs: :shifts).where.not(shifts: { user_id: nil }).group('addresses.state').sum('shifts.duration') }
-  scope :volunteer_minutes_by_county, ->(state) { joins(:address, needs: :shifts).where.not(shifts: { user_id: nil }).where(addresses: { state: state }).group('offices.id, addresses.county').sum('shifts.duration') }
-  # scope :children_served, -> { joins(needs: :shifts).where.not(shifts: { user_id: nil }).group('needs.id').sum('needs.number_of_children') }
-  # scope :children_served_by_state, -> { joins(:address, :needs).group('offices.id, addresses.state').sum('needs.number_of_children')}
-  # scope :children_served_by_county, ->(state) { joins(:address, needs: :shifts).where.not(shifts: { user_id: nil }).where(addresses: { state: state }).group('offices.id, addresses.county').sum('needs.number_of_children')}
+  scope :volunteer_minutes_by_county, ->(state) { joins(:address, needs: :shifts).where.not(shifts: { user_id: nil }).where(addresses: { state: state }).group('offices.id, addresses.county').sum(:duration) }
+  scope :children_served, -> { joins(needs: :shifts).where.not(shifts: { user_id: nil }).sum('DISTINCT ON (needs.office_id) needs.number_of_children') }
+  scope :children_served_by_state, -> { joins(:address, :needs).group('offices.id, addresses.state').sum('needs.number_of_children')}
+  scope :children_served_by_county, ->(state) { joins(:address, needs: :shifts).where.not(shifts: { user_id: nil }).where(addresses: { state: state }).group('offices.id, addresses.county').sum('needs.number_of_children')}
   # scope :children_by_demographic, -> { joins(needs: :preferred_language).group('offices.id, languages.name').count('languages.name') }
   # scope :volunteers_by_demographic, -> { joins(users: :race).group('races.name').count('users.id') }
 end
