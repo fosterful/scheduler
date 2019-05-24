ca8149e1905c4e0e3f1ae1536aa504c5d50a1e0f
Kyle Boe <ksboe1@gmail.com>
2019-05-24 14:46:50 -0700

    update naming and params of specs - #27

--- spec/requests/admin/admin_reports_spec.rb
+++ spec/requests/admin/admin_reports_spec.rb
@@ -23,72 +23,72 @@ RSpec.describe 'Admin reports spec', type: :request do
     end
 
     describe '#total_volunteer_minutes_by_office' do
-      it 'does a thing' do
-        get admin_reports_total_volunteer_minutes_by_office_path
-        expect(response.body).to include('office_id,volunteer_minutes')
+      it 'returns total volunteer minutes grouped by office' do
+        get admin_reports_total_volunteer_minutes_by_office_path(:csv)
+        expect(response.body).to include('Office ID', 'Volunteer Minutes')
       end
     end
 
     describe '#total_volunteer_minutes_by_state' do
-      it 'does something' do
-        get admin_reports_total_volunteer_minutes_by_state_path
-        expect(response.body).to include('state,volunteer_minutes')
+      it 'returns total volunteer minutes grouped by state' do
+        get admin_reports_total_volunteer_minutes_by_state_path(:csv)
+        expect(response.body).to include('State', 'Volunteer Minutes')
       end
     end
 
     describe '#total_volunteer_minutes_by_county' do
-      it 'does something' do
-        get admin_reports_total_volunteer_minutes_by_county_path
-        expect(response.body).to include('county,volunteer_minutes')
+      it 'returns total volunteer minutes grouped by county' do
+        get admin_reports_total_volunteer_minutes_by_county_path(:csv), params: { state: 'WA' }
+        expect(response.body).to include('County', 'Volunteer Minutes')
       end
     end
 
     describe '#total_children_served_by_office' do
-      it 'does something' do
-        get admin_reports_total_children_served_by_office_path
-        expect(response.body).to include('office_id,children_served')
+      it 'returns total children served grouped by office' do
+        get admin_reports_total_children_served_by_office_path(:csv)
+        expect(response.body).to include('Office ID', 'Children Served')
       end
     end
 
     describe '#total_children_served_by_state' do
-      it 'does something' do
-        get admin_reports_total_children_served_by_state_path
-        expect(response.body).to include('state,children_served')
+      it 'returns total children served grouped by state' do
+        get admin_reports_total_children_served_by_state_path(:csv)
+        expect(response.body).to include('State', 'Children Served')
       end
     end
 
     describe '#total_children_served_by_county' do
-      it 'does something' do
-        get admin_reports_total_children_served_by_county_path
-        expect(response.body).to include('county,children_served')
+      it 'returns total children served grouped by county' do
+        get admin_reports_total_children_served_by_county_path(:csv), params: { state: 'WA' }
+        expect(response.body).to include('County', 'Children Served')
       end
     end
 
     describe '#total_children_by_demographic' do
-      it 'does something' do
-        get admin_reports_total_children_by_demographic_path
-        expect(response.body).to include('preferred_language,number_of_children')
+      it 'returns total children grouped by demographic' do
+        get admin_reports_total_children_by_demographic_path(:csv)
+        expect(response.body).to include('Preferred Language', 'Number of Children')
       end
     end
 
     describe '#total_volunteers_by_race' do
-      it 'does something' do
-        get admin_reports_total_volunteers_by_race_path
-        expect(response.body).to include('race,number_of_volunteers')
+      it 'returns total volunteers grouped by race' do
+        get admin_reports_total_volunteers_by_race_path(:csv)
+        expect(response.body).to include('Race', 'Number of Volunteers')
       end
     end
 
     describe '#total_volunteer_minutes_by_user' do
-      it 'does something' do
-        get admin_reports_total_volunteer_minutes_by_user_path
-        expect(response.body).to include('user_id,total_volunteer_minutes')
+      it 'returns total volunteer minutes grouped by user' do
+        get admin_reports_total_volunteer_minutes_by_user_path(:csv)
+        expect(response.body).to include('User ID', 'Total Volunteer Minutes')
       end
     end
 
     describe '#total_volunteers_by_spoken_language' do
-      it 'does something' do
-        get admin_reports_total_volunteers_by_spoken_language_path
-        expect(response.body).to include('language,total_volunteer_minutes')
+      it 'returns total volunteers grouped by spoken language' do
+        get admin_reports_total_volunteers_by_spoken_language_path(:csv)
+        expect(response.body).to include('Language', 'Total Volunteer Minutes')
       end
     end
   end
