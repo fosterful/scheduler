be42440d1277447e98d0a9a90fa37b81b9f8a560
Kyle Boe <ksboe1@gmail.com>
2019-05-24 14:45:37 -0700

    change html response to csv formatted response - #27

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -7,84 +7,129 @@ module Admin
     def index; end
 
     def total_volunteer_minutes_by_office
+      @headers = ['Office ID', 'Volunteer Minutes']
+      @data = Office.total_volunteer_minutes_by_office
       respond_to do |format|
-        format.html { send_data generate_csv(%w(office_id volunteer_minutes), Office.total_volunteer_minutes_by_office), filename: "#{DateTime.current.to_i}-total-volunteer-minutes-by-office.csv" }
-        format.json { render json: Office.total_volunteer_minutes_by_office.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteer-minutes-by-office')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_volunteer_minutes_by_state
+      @headers = ['State', 'Volunteer Minutes']
+      @data = Office.total_volunteer_minutes_by_state
       respond_to do |format|
-        format.html { send_data generate_csv(%w(state volunteer_minutes), Office.total_volunteer_minutes_by_state), filename: "#{DateTime.current.to_i}-total-volunteer-minutes-by-state.csv" }
-        format.json { render json: Office.total_volunteer_minutes_by_state.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteer-minutes-by-state')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_volunteer_minutes_by_county
+      @headers = ['County', 'Volunteer Minutes']
+      @data = Office.total_volunteer_minutes_by_county(params['state'])
       respond_to do |format|
-        format.html { send_data generate_csv(%w(county volunteer_minutes), Office.total_volunteer_minutes_by_county(params['state'])), filename: "#{DateTime.current.to_i}-total-volunteer-minutes-by-county.csv" }
-        format.json { render json: Office.total_volunteer_minutes_by_county(params['state']).to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteer-minutes-by-county')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_children_served_by_office
+      @headers = ['Office ID', 'Children Served']
+      @data = Office.total_children_served_by_office
       respond_to do |format|
-        format.html { send_data generate_csv(%w(office_id children_served), Office.total_children_served_by_office), filename: "#{DateTime.current.to_i}-total-children-served-by-office.csv" }
-        format.json { render json: Office.total_children_served_by_office.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-children-served-by-office')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_children_served_by_state
+      @headers = ['State', 'Children Served']
+      @data = Office.total_children_served_by_state
       respond_to do |format|
-        format.html { send_data generate_csv(%w(state children_served), Office.total_children_served_by_state), filename: "#{DateTime.current.to_i}-total-children-served-by-state.csv" }
-        format.json { render json: Office.total_children_served_by_state.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-children-served-by-state')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_children_served_by_county
+      @headers = ['County', 'Children Served']
+      @data = Office.total_children_served_by_county(params['state'])
       respond_to do |format|
-        format.html { send_data generate_csv(%w(county children_served), Office.total_children_served_by_county(params['state'])), filename: "#{DateTime.current.to_i}-total-children-served-by-county.csv" }
-        format.json { render json: Office.total_children_served_by_county(params['state']).to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-children-served-by-county')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_children_by_demographic
+      @headers = ['Preferred Language', 'Number of Children']
+      @data = Office.total_children_by_demographic
       respond_to do |format|
-        format.html { send_data generate_csv(%w(preferred_language number_of_children), Office.total_children_by_demographic), filename: "#{DateTime.current.to_i}-total-children-by-demographic.csv" }
-        format.json { render json: Office.total_children_by_demographic.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-children-by-demographic')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_volunteers_by_race
+      @headers = ['Race', 'Number of Volunteers']
+      @data = Office.total_volunteers_by_race
       respond_to do |format|
-        format.html { send_data generate_csv(%w(race number_of_volunteers), Office.total_volunteers_by_race), filename: "#{DateTime.current.to_i}-total-volunteers-by-race.csv" }
-        format.json { render json: Office.total_volunteers_by_race.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteers-by-race')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_volunteer_minutes_by_user
+      @headers = ['User ID', 'Total Volunteer Minutes']
+      @data = User.total_volunteer_minutes_by_user
       respond_to do |format|
-        format.html { send_data generate_csv(%w(user_id total_volunteer_minutes), User.total_volunteer_minutes_by_user), filename: "#{DateTime.current.to_i}-total-volunteer-minutes-by-user.csv" }
-        format.json { render json: User.total_volunteer_minutes_by_user.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteer-minutes-by-user')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     def total_volunteers_by_spoken_language
+      @headers = ['Language', 'Total Volunteer Minutes']
+      @data = User.total_volunteers_by_spoken_language
       respond_to do |format|
-        format.html { send_data generate_csv(%w(language total_volunteer_minutes), User.total_volunteers_by_spoken_language), filename: "#{DateTime.current.to_i}-total-volunteers-by-spoken-language.csv" }
-        format.json { render json: User.total_volunteers_by_spoken_language.to_json }
+        format.csv do
+          headers['Content-Disposition'] = content_disposition('total-volunteers-by-spoken-language')
+          headers['Content-Type'] = 'text/csv'
+        end
+        format.json { render json: @data.to_json }
       end
     end
 
     private
 
-    def generate_csv(column_labels, data)
-      CSV.generate(headers: true) do |csv|
-        csv << column_labels
-        data.each do |k, v|
-          csv << [k, v]
-        end
-      end
+    def content_disposition(report_name)
+      "attachment; filename=\"#{DateTime.current.to_i}-#{report_name}.csv\""
     end
 
     def find_resource(_param); end
