c7f2b89cee7b89186669aef44f77bd088f9bae97
Benjamin Wood <benjaminwood@gmail.com>
2019-05-24 12:53:13 -0700

    Fix spec

--- spec/workers/send_text_message_worker_spec.rb
+++ spec/workers/send_text_message_worker_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe SendTextMessageWorker do
   describe '#perform_async' do
-    subject { described_class.perform_async('123-456-7890', 'Hello World') }
+    subject { described_class.perform_async('360-610-7089', 'Hello World') }
 
     it 'enqueues the worker' do
       expect { subject }.to change(described_class.jobs, :size).by(1)
@@ -14,13 +14,21 @@ RSpec.describe SendTextMessageWorker do
   describe '#perform' do
     it 'calls to twilio api' do
       expect($twilio).to receive_message_chain(:api, :account, :messages, :create)
-      described_class.new.perform('123-456-7890', 'Hello World')
+      described_class.new.perform('360-610-7089', 'Hello World')
     end
 
     context 'when the number is missing' do
       it 'does not call the twilio api' do
         allow($twilio).to receive(:api)
         described_class.new.perform(nil, 'Hello World')
+        expect($twilio).not_to have_received(:api)
+      end
+    end
+
+    context 'when the number is missing' do
+      it 'does not call the twilio api' do
+        allow($twilio).to receive(:api)
+        described_class.new.perform('123-456-7890', 'Hello World')
         expect($twilio).not_to have_received(:api)
       end
     end
