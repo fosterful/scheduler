bfb56b491a4228045729df8a93121203ca430ea4
Benjamin Wood <benjaminwood@gmail.com>
2019-05-24 10:19:45 -0700

    Add guard clause for nil number as stop gap solution - #85

--- app/workers/send_text_message_worker.rb
+++ app/workers/send_text_message_worker.rb
@@ -4,6 +4,11 @@ class SendTextMessageWorker
   include Sidekiq::Worker
 
   def perform(number, message)
+    # TODO: remove guard clause
+    # once notification refactor
+    # is complete.
+    return if number.blank?
+
     if Rails.env.production?
       $twilio.api.account.messages.create(messaging_service_sid: Rails.configuration.twilio_messaging_service_sid, to: number, body: message)
     else
