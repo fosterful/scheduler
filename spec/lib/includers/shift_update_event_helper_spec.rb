70f83d551e2255e42dff8933cbaaebcdf58af571
Jason Dinsmore <jason@dinjas.com>
2019-06-12 19:38:40 -0700

    Flesh out spec for helper module

--- spec/lib/includers/shift_update_event_helper_spec.rb
+++ spec/lib/includers/shift_update_event_helper_spec.rb
@@ -3,63 +3,233 @@
 require 'rails_helper'
 
 RSpec.describe ShiftUpdateEventHelper do
+
+  let(:shift) { create(:shift) }
+  let(:need) { shift.need }
+  let(:volunteer1) do
+    u = create(:user)
+    u.offices << shift.office
+    u
+  end
+  let(:volunteer2) do
+    u = create(:user)
+    u.offices << shift.office
+    u
+  end
   let(:object) do
-    Class.new do
-      include described_class
-    end
+    double(Shift,
+           current_user: volunteer1,
+           user_was:     nil,
+           need:         need,
+           user:         shift.user).extend(described_class)
+  end
+  let(:social_worker1) do
+    volunteer1.update!(role: User::SOCIAL_WORKER)
+    volunteer1
+  end
+  let(:social_worker2) do
+    volunteer2.update!(role: User::SOCIAL_WORKER)
+    volunteer2
   end
 
-  # TODO: auto-generated
+
   describe '#shift_user_is_current_user?' do
-    it 'shift_user_is_current_user?' do
+    it 'returns false when shift does not have a user' do
       result = object.shift_user_is_current_user?
 
-      expect(result).not_to be_nil
+      expect(result).to be false
+    end
+
+    it 'returns true when shift user is current user' do
+      shift.user = volunteer1
+      shift.save!
+
+      result = object.shift_user_is_current_user?
+
+      expect(result).to be true
+    end
+
+    it 'returns false when current user is not shift user' do
+      allow(object).to receive(:current_user).and_return(volunteer2)
+
+      result = object.shift_user_is_current_user?
+
+      expect(result).to be false
     end
   end
 
-  # TODO: auto-generated
   describe '#current_user_left_shift?' do
-    it 'current_user_left_shift?' do
+    it 'returns false if current_user is assigned to shift' do
       result = object.current_user_left_shift?
 
-      expect(result).not_to be_nil
+      expect(result).to be false
+    end
+
+    it 'returns false if user assigned to shift, but user_was is nil' do
+      allow(object).to receive(:user_was).and_return(nil)
+
+      result = object.current_user_left_shift?
+
+      expect(result).to be false
+    end
+
+    it 'returns false if user_was is not current_user' do
+      allow(object).to receive(:user_was).and_return(volunteer2)
+
+      result = object.current_user_left_shift?
+
+      expect(result).to be false
+    end
+
+    it 'returns false if user_was is nil and no user assigned to shift' do
+      allow(object).to receive(:user).and_return(nil)
+
+      result = object.current_user_left_shift?
+
+      expect(result).to be false
+    end
+
+    it 'returns true if user nil, and current_user is user_was' do
+      allow(object).to receive(:user).and_return(nil)
+      allow(object).to receive(:user_was).and_return(volunteer1)
+
+      result = object.current_user_left_shift?
+
+      expect(result).to be true
+    end
+
+    it 'returns true if user present and current_user is user_was' do
+      allow(object).to receive(:user_was).and_return(volunteer1)
+
+      result = object.current_user_left_shift?
+
+      expect(result).to be true
     end
   end
 
-  # TODO: auto-generated
   describe '#scheduler_with_user?' do
-    it 'scheduler_with_user?' do
-      result = object.scheduler_with_user?
+    context 'when current user is not a scheduler' do
+      it 'returns false if shift not assigned' do
+        allow(object).to receive(:user).and_return(nil)
 
-      expect(result).not_to be_nil
+        result = object.scheduler_with_user?
+
+        expect(result).to be false
+      end
+
+      it 'returns false if shift assigned' do
+        shift.user = volunteer2
+        shift.save!
+
+        result = object.scheduler_with_user?
+
+        expect(result).to be false
+      end
+    end
+
+    context 'when current_user is a scheduler' do
+      before do
+        volunteer1.update!(role: User::COORDINATOR)
+      end
+
+      it 'returns false if shift not assigned' do
+        allow(object).to receive(:user).and_return(nil)
+
+        result = object.scheduler_with_user?
+
+        expect(result).to be false
+      end
+
+      it 'returns true if shift assigned' do
+        shift.user = volunteer2
+        shift.save!
+
+        result = object.scheduler_with_user?
+
+        expect(result).to be true
+      end
     end
   end
 
-  # TODO: auto-generated
   describe '#scheduler_without_user?' do
-    it 'scheduler_without_user?' do
-      result = object.scheduler_without_user?
+    context 'when current user is not a scheduler' do
+      it 'returns false if shift not assigned' do
+        allow(object).to receive(:user).and_return(nil)
 
-      expect(result).not_to be_nil
+        result = object.scheduler_without_user?
+
+        expect(result).to be false
+      end
+
+      it 'returns false if shift assigned' do
+        shift.user = volunteer2
+        shift.save!
+
+        result = object.scheduler_without_user?
+
+        expect(result).to be false
+      end
+    end
+
+    context 'when current_user is a scheduler' do
+      before do
+        volunteer1.update!(role: User::COORDINATOR)
+      end
+
+      it 'returns true if shift not assigned' do
+        allow(object).to receive(:user).and_return(nil)
+
+        result = object.scheduler_without_user?
+
+        expect(result).to be true
+      end
+
+      it 'returns false if shift assigned' do
+        shift.user = volunteer2
+        shift.save!
+
+        result = object.scheduler_without_user?
+
+        expect(result).to be false
+      end
     end
   end
 
-  # TODO: auto-generated
   describe '#social_workers_and_need_user' do
-    it 'social_workers_and_need_user' do
+    it 'returns need user and all social workers associated with the office' do
+      volunteer1.update!(role: User::SOCIAL_WORKER)
+      volunteer2.update!(role: User::SOCIAL_WORKER)
+
       result = object.social_workers_and_need_user
 
-      expect(result).not_to be_nil
+      expect(result).to match_array([volunteer1, volunteer2, need.user])
+    end
+
+    it 'returns need user if no social workers' do
+      result = object.social_workers_and_need_user
+
+      expect(result).to match_array([need.user])
     end
   end
 
-  # TODO: auto-generated
   describe '#social_workers' do
-    it 'social_workers' do
+    before do
+      need.user.update!(role: User::VOLUNTEER)
+    end
+
+    it 'returns all social workers associated with the office' do
+      social_worker1
+      social_worker2
+
       result = object.social_workers
 
-      expect(result).not_to be_nil
+      expect(result).to match_array([social_worker1, social_worker2])
+    end
+
+    it 'returns empty collection if no social workers' do
+      result = object.social_workers
+
+      expect(result).to match_array([])
     end
   end
 
