c99f9174bf7b1644f24e117cc246470083b2ac62
FatherofDharma <49597814+FatherofDharma@users.noreply.github.com>
2019-10-30 08:37:09 -0700

    Remove commented out debug code from app/helpers/needs_helper.rb
    Co-Authored-By: Benjamin Wood <benjaminwood@gmail.com>

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -7,7 +7,6 @@ module NeedsHelper
   def language_options_for_select
     language_options = Language.all.map { |l| [l.name, l.id] }
     english = language_options.find { |l| l[0]=='English'}
-    #raise english.inspect
     options_for_select(language_options, selected: english[1])
   end
 end
