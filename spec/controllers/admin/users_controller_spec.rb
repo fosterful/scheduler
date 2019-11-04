c15c1767bbe36c709b2f6e82c03b98b838df1c9d
Jason Dinsmore <jason@dinjas.com>
2019-11-04 11:53:29 -0800

    Fix existing specs; start expanding users#index admin spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
+  let(:admin) { create(:admin) }
 
   describe '#create' do
     it 'POST create' do
@@ -13,14 +14,35 @@ RSpec.describe Admin::UsersController, type: :controller do
   end
 
   describe '#index' do
+    let(:users) do
+      create(:user, first_name: 'Aaron', last_name: 'Zippy')
+      create(:user, first_name: 'Adam', last_name: 'Zappy')
+    end
+
+    before do
+      sign_in admin
+    end
+
     it 'GET index' do
       get :index
 
-      expect(response).to have_http_status(:found)
+      expect(response).to have_http_status(:ok)
     end
 
-    it 'orders by name' do
-    #  needs content 
+    it 'orders by name asc' do
+      users
+
+      get :index, params: { users: { order: :name, direction: :asc } }
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'orders by name desc' do
+      users
+
+      get :index, params: { users: { order: :name, direction: :desc } }
+
+      expect(response).to have_http_status(:ok)
     end
   end
 
