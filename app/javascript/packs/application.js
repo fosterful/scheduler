bf270ced599bf3dbd5b4d3f364245a04e8f466bf
Joel Hayhurst <joelmichael@gmail.com>
2019-11-07 16:39:02 -0800

    Replace @babel/polyfill
    The new system is to include these two lines instead.
    See:
    https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import Foundation from '../src/foundation_loader'
 import '../src/application.scss'
 import Rails from 'rails-ujs'
