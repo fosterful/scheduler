bfd221ad448c16931c7d567f8b7932e449e55129
Joel Hayhurst <joelmichael@gmail.com>
2019-11-12 15:21:48 -0800

    Add optout uniqueness validation and spec

--- app/models/optout.rb
+++ app/models/optout.rb
@@ -1,4 +1,6 @@
 class Optout < ApplicationRecord
   belongs_to :user
   belongs_to :need
+
+  validates :need_id, uniqueness: { scope: :user_id }
 end
--- spec/models/optout_spec.rb
+++ spec/models/optout_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Optout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:need) { create(:need) }
+
+  it 'should validate uniqueness of user and need' do
+    create(:optout, user: user, need: need)
+    expect {
+      create(:optout, user: user, need: need)
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
