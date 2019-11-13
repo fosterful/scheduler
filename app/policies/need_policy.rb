050d6de8a4a94271405452f56e13ffa1dafe0485
FatherofDharma <fatherofdharma@gmail.com>
2019-11-13 14:26:07 -0800

    add notes to permitted_attributes

--- app/policies/need_policy.rb
+++ app/policies/need_policy.rb
@@ -34,7 +34,7 @@ class NeedPolicy < ApplicationPolicy
   end
 
   def permitted_attributes
-    [:race_id, :preferred_language_id, :number_of_children, age_range_ids: []]
+    [:race_id, :preferred_language_id, :number_of_children, :notes, age_range_ids: []]
   end
 
   class Scope < Scope
