971203a10067e97deaff91ad17768d7b938414f9
Joel Hayhurst <joelmichael@gmail.com>
2019-11-13 00:07:50 +0000

    Add Optout policy and spec

---
+++ app/policies/optout_policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class OptoutPolicy < ApplicationPolicy
+  def create?
+    user.admin? || user_in_office?
+  end
+
+  private
+
+  def user_in_office?
+    record.need.office&.user_ids&.include? user.id
+  end
+end
---
+++ spec/policies/optout_policy_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe OptoutPolicy do
+  subject { described_class.new(user, record) }
+
+  let(:record) { create :optout, user: user, need: need }
+  let(:user)   { build(:user) }
+  let(:need)   { build(:need) }
+
+  context 'when user is an admin' do
+    let(:user) { create(:user, role: 'admin') }
+
+    it { is_expected.to permit_action(:create) }
+  end
+
+  context 'when user is a volunteer' do
+    let(:user) { create :user, role: 'volunteer' }
+
+    context 'when in the office' do
+      before { record.need.office.users << user }
+
+      it { is_expected.to permit_action(:create) }
+    end
+
+    context 'when not in the office' do
+      it { is_expected.to forbid_action(:create) }
+    end
+  end
+end
