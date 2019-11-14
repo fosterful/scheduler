3cba915dd75d35683ab8206a84d48b5d56324e0c
Joel Hayhurst <joelmichael@gmail.com>
2019-11-13 16:59:47 -0800

    Link number instead of whole sentence

--- app/javascript/src/responses.js
+++ app/javascript/src/responses.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $('#toggle-optouts-list').on('click', function() {
+  $('#toggle-optouts-list').on('click', function(e) {
+    e.preventDefault();
     $('#optouts-list').toggleClass('hide');
   });
 
-  $('#toggle-pending-list').on('click', function() {
+  $('#toggle-pending-list').on('click', function(e) {
+    e.preventDefault();
     $('#pending-list').toggleClass('hide');
   });
 });
