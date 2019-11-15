4d4953652fb7be21927f3d9d4d835c88362c6cd3
FatherofDharma <fatherofdharma@gmail.com>
2019-11-15 11:07:10 -0800

    add user resource for dashboard

---
+++ app/admin/users.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register User do
+
+  filter :first_name
+  filter :last_name
+  filter :email
+  filter :offices
+
+  index do
+    column :name
+    column :email
+    column :offices
+    id_column
+    actions
+  end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :role, :invitation_token, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at, :invitation_limit, :invited_by_type, :invited_by_id, :invitations_count, :first_name, :last_name, :birth_date, :phone, :resident_since, :discovered_omd_by, :medical_limitations, :medical_limitations_desc, :conviction, :conviction_desc, :time_zone, :race_id, :first_language_id, :second_language_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :role, :invitation_token, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at, :invitation_limit, :invited_by_type, :invited_by_id, :invitations_count, :first_name, :last_name, :birth_date, :phone, :resident_since, :discovered_omd_by, :medical_limitations, :medical_limitations_desc, :conviction, :conviction_desc, :time_zone, :race_id, :first_language_id, :second_language_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
