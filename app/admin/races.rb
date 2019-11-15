5229c34e513dcb977372ee97b98d9977b477db26
FatherofDharma <fatherofdharma@gmail.com>
2019-11-15 11:46:34 -0800

    add race resource to dashboard

---
+++ app/admin/races.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Race do
+
+  filter :id
+  filter :name
+
+  index do
+    id_column
+    column :name
+    actions
+  end
+
+   menu priority: 4
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
