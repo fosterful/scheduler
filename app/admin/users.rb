4bcff19f620a78f0c950765d113ad906deb3f622
FatherofDharma <fatherofdharma@gmail.com>
2019-11-19 15:29:55 -0800

    add custom form for creating user and alter create resource method

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,6 +7,8 @@ ActiveAdmin.register User do
   filter :email
   filter :offices
 
+  form partial: 'form'
+
   index do
     id_column
     column :name
@@ -16,6 +18,12 @@ ActiveAdmin.register User do
   end
 
   menu priority: 1
+
+  controller do
+    def create_resource(object)
+      object.invite!(current_user)
+    end
+  end
   # menu parent: 'offices'
 
   # See permitted parameters documentation:
@@ -23,8 +31,9 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
+
   # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :role, :invitation_token, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at, :invitation_limit, :invited_by_type, :invited_by_id, :invitations_count, :first_name, :last_name, :birth_date, :phone, :resident_since, :discovered_omd_by, :medical_limitations, :medical_limitations_desc, :conviction, :conviction_desc, :time_zone, :race_id, :first_language_id, :second_language_id
-  #
+  
   # or
   #
   # permit_params do
