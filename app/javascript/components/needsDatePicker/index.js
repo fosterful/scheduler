221b65d243a5a5fc3f2521d286ee8c461062ec23
Mike Lofgren <mikeylofgren@gmail.com>
2019-11-20 14:13:02 -0800

    start datepicker.

---
+++ app/javascript/components/needsDatePicker/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Calendar from 'startAt/Calendar'
+
+class NeedsDatePicker extends React.Component{
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+NeedsDatePicker.propTypes = {
+  
+}
+export default NeedsDatePicker
