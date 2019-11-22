d40fa303b30c1b7a7d6904294d4bdbbd41dbff96
Mike Lofgren <mikeylofgren@gmail.com>
2019-11-21 16:12:28 -0800

    work on date time component.

---
+++ app/javascript/components/needsPicker/addNeedsPicker/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import NeedsPickerContext from "needsPicker/contexts/picker";
+import Calendar from "needsPicker/Calendar";
+import "react-day-picker/lib/style.css";
+
+class AddNeedsPicker extends React.Component {
+  setCalendarMonth = calendarMonth =>
+    this.setState({
+      calendarMonth: moment(calendarMonth).startOf("month")
+    });
+
+  state = {
+    calendarMonth: moment().startOf("month"),
+    setCalendarMonth: this.setCalendarMonth,
+    makeRequest: makeRequestFn(this.props.authenticity_token)
+  };
+
+  render() {
+    const {
+      state: { calendarMonth }
+    } = this;
+    return (
+      <NeedsPickerContext.Provider
+        value={{
+          ...this.state,
+          ...{ authenticityToken: this.props.authenticity_token }
+        }}
+      >
+        <React.Fragment>
+          <Calendar />
+        </React.Fragment>
+      </NeedsPickerContext.Provider>
+    );
+  }
+}
+
+AddNeedsPicker.propTypes = {};
+export default AddNeedsPicker;
