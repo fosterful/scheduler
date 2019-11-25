ccfa801f38efd40afbce368a3a04b4b971e4d2cc
Mike Lofgren <mikeylofgren@gmail.com>
2019-11-25 14:27:58 -0800

    switch datepicker from react-date-picker to react-datetime.

---
+++ app/javascript/components/NewNeedDatePicker/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DateTime from "react-datetime";
+
+class AddNeedsPicker extends React.Component {
+  render() {
+    return (
+      <DateTime
+        dateFormat='DD-MM-YYYY'
+        inputProps={{ name: "need[start_at]" }}
+        timeConstraints={{ minutes: { step: 15 } }}
+      />
+    );
+  }
+}
+
+AddNeedsPicker.propTypes = {};
+export default AddNeedsPicker;
--- app/javascript/components/needsPicker/contexts/picker.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default React.createContext({
-  calendarMonth: null,
-  setCalendarMonth: _ => {},
-  authenticityToken: ""
-});
