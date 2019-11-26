2d5523d3a63652b2752d3a021dcd82067a864eca
FatherofDharma <fatherofdharma@gmail.com>
2019-11-25 16:09:17 -0800

    remove unused specs and files from administrate

--- spec/controllers/admin/age_ranges_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Admin::AgeRangesController, type: :controller do
-
-end
--- spec/controllers/admin/races_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Admin::RacesController, type: :controller do
-
-end
