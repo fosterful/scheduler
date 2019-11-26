5d9d297563c832fc5244ef40425635fc71ee77f1
Benjamin Wood <benjaminwood@gmail.com>
2019-11-26 10:52:23 -0800

    More prettier

--- app/javascript/components/NewNeedDatePicker/index.js
+++ app/javascript/components/NewNeedDatePicker/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
-import DateTime from "react-datetime";
+import React from 'react'
+import PropTypes from 'prop-types'
+import DateTime from 'react-datetime'
 
 class AddNeedsPicker extends React.Component {
   render() {
     return (
       <DateTime
         dateFormat='DD-MM-YYYY'
-        inputProps={{ name: "need[start_at]" }}
+        inputProps={{ name: 'need[start_at]' }}
         timeConstraints={{ minutes: { step: 15 } }}
       />
-    );
+    )
   }
 }
 
-AddNeedsPicker.propTypes = {};
-export default AddNeedsPicker;
+AddNeedsPicker.propTypes = {}
+export default AddNeedsPicker
