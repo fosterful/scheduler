120145f2a7c4790b2aed243fdc0e1fe690b2d6c5
Joel Hayhurst <joelmichael@gmail.com>
2019-12-06 16:13:04 -0800

    Refactor reminder code
    Use a separate where instead of filtering out needs with a "next".
    Split up the code a bit to reduce cognitive complexity.

--- app/workers/remind_worker.rb
+++ app/workers/remind_worker.rb
@@ -3,26 +3,36 @@ class RemindWorker
   include Rails.application.routes.url_helpers
 
   def perform
-    needs = Need.joins(:shifts).
-      where("shifts.created_at < ?", 1.hour.ago).
-      where("shifts.start_at > ?", Time.now).
-      where("shifts.user_id IS NULL").distinct
-    needs.each do |need|
-      # Only send one message a day until within the day of
-      if need.created_at < 12.hours.ago &&
-        need.effective_start_at > 12.hours.from_now
-        next unless Time.current.hour == 12
-      end
-      send_message(need)
+    send_daily_messages
+    send_hourly_messages
+  end
+
+  def send_daily_messages
+    if Time.current.hour == 12 # only send at noon
+      needs = Need.joins(:shifts).
+        where("shifts.start_at > ?", 12.hours.from_now).
+        where("needs.created_at < ?", 12.hours.ago).
+        where("shifts.user_id IS NULL").distinct
+      send_messages(needs)
     end
   end
 
-  def send_message(need)
-    recipients = need.users_pending_response
-    if recipients.any?
-      phone_numbers = recipients.map(&:phone)
-      message = "A need still has available shifts #{need_url(need)}"
-      Services::TextMessageEnqueue.send_messages(phone_numbers, message)
+  def send_hourly_messages
+    needs = Need.joins(:shifts).
+      where("shifts.created_at BETWEEN ? AND ?", 12.hours.ago, 1.hour.ago).
+      where("shifts.start_at BETWEEN ? AND ?", Time.now, 12.hours.from_now).
+      where("shifts.user_id IS NULL").distinct
+    send_messages(needs)
+  end
+
+  def send_messages(needs)
+    needs.each do |need|
+      recipients = need.users_pending_response
+      if recipients.any?
+        phone_numbers = recipients.map(&:phone)
+        message = "A need still has available shifts #{need_url(need)}"
+        Services::TextMessageEnqueue.send_messages(phone_numbers, message)
+      end
     end
   end
 end
