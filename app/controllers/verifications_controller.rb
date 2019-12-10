5d84a41f14b41c84e465386fb717f44d3bf9b6d2
Joel Hayhurst <joelmichael@gmail.com>
2019-12-10 13:36:11 -0800

    Add Verify SID to production credentials

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,11 +1,11 @@
 class VerificationsController < ApplicationController
-  TWILIO_VERIFY_SID = ENV['TWILIO_VERIFY_SID']
+  VERIFY_SID = Rails.application.credentials.twilio[:verify_sid]
 
   def index
   end
 
   def send_code
-    $twilio.verify.services(TWILIO_VERIFY_SID).verifications.
+    $twilio.verify.services(VERIFY_SID).verifications.
       create(to: twilio_phone_number, channel: 'sms')
     flash[:notice] = 'Verification code has been sent'
     redirect_to verify_path
@@ -14,11 +14,11 @@ class VerificationsController < ApplicationController
   def check_code
     params.permit(:code)
     code = params[:code]
-    check = $twilio.verify.services(TWILIO_VERIFY_SID).verification_checks.
+    check = $twilio.verify.services(VERIFY_SID).verification_checks.
       create(to: twilio_phone_number, code: code)
     if check.status == 'approved'
       current_user.update(verified: true)
-      flash[:notice] = 'Phone number has been verified'
+      flash[:notice] = 'Phone number has been verified!'
     else
       flash[:notice] = 'Verification failed'
     end
