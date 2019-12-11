98944b831e33f8db3308e2ffb6952436ab0a5404
Joel Hayhurst <joelmichael@gmail.com>
2019-12-11 08:55:12 -0800

    Remove sleep and add rescue block
    There might be some un-anticipated errors (based on phone number
    formatting, for example), this rescue block will print
    them out to the logs rather than stopping the migration.

--- db/migrate/20191209221008_pre_verify_eligible_users.rb
+++ db/migrate/20191209221008_pre_verify_eligible_users.rb
@@ -1,16 +1,19 @@
 class PreVerifyEligibleUsers < ActiveRecord::Migration[6.0]
   def up
     User.where.not(phone: nil).find_each do |user|
-      sleep 1
-      messages = $twilio.messages.list(to: user.phone, limit: 100)
-      messages.each do |m|
-        if m.status == 'delivered'
-          user.update! verified: true
-          puts "marked user #{user.id} as verified"
-          break
+      begin
+        messages = $twilio.messages.list(to: user.phone, limit: 100)
+        messages.each do |m|
+          if m.status == 'delivered'
+            user.update! verified: true
+            puts "marked user #{user.id} as verified"
+            break
+          end
         end
+        puts "could not find delivered message for #{user.id}"
+      rescue Exception => e
+        puts "error while processing user #{user.id}: #{e}"
       end
-      puts "could not find delivered message for #{user.id}"
     end
   end
 end
