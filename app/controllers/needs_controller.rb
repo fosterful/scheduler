399d30b7ac946d6b1d5a7a5ea40f8922e3c615f6
Joel Hayhurst <joelmichael@gmail.com>
2019-12-16 09:44:21 -0800

    Use admin? again

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -6,7 +6,7 @@ class NeedsController < ApplicationController
 
     @needs = policy_scope(Need).includes(:shifts).order(:start_at)
 
-    if current_user.role == 'admin' && params[:date]
+    if current_user.admin? && params[:date]
       @date = Date.parse(params[:date])
       @needs = @needs.on_date(@date)
     else
