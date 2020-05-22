1a4e02a815b1d5765579356f2c10a4420ca303d2
Jason Dinsmore <jason@dinjas.com>
2020-05-22 11:54:44 -0700

    Add validations to Annoucement model

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class Announcement < ApplicationRecord
   belongs_to :author
+
+  validates :message, presence: true
+  validates :author_id, presence: true
+  validates :user_ids, presence: true
 end
