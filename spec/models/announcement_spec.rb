c05a3cd58e859cf8a93d9a5b22cb237eef0df66a
Benjamin Wood <benjaminwood@gmail.com>
2020-05-29 22:42:17 +0000

    Add another example for #send_messages

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -8,16 +8,26 @@ RSpec.describe Announcement, type: :model do
   end
 
   describe '#send_messages' do
-    let(:announcement) { create :announcement }
     let(:phone_numbers) { ['(360) 610-7089'] }
 
-    it 'send_messages' do
+    it 'does not send messages if not persisted' do
       expect(Services::TextMessageEnqueue)
-        .to receive(:send_messages)
-              .with(phone_numbers, 'MyText')
-              .and_call_original
+        .not_to receive(:send_messages)
 
       announcement.send_messages
+    end
+
+    context 'when the annoucement is persisted' do
+      let(:announcement) { create :announcement }
+
+      it 'sends the messages' do
+        expect(Services::TextMessageEnqueue)
+          .to receive(:send_messages)
+                .with(phone_numbers, 'MyText')
+                .and_call_original
+  
+        announcement.send_messages
+      end
     end
   end
 
