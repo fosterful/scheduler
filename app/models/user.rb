99457b3951485b45817cc4d02b3ab5c02e0ca549
nshoes <nate@hint.io>
2020-07-28 17:27:39 +0000

    move extend to top of class definition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  extend DateRangeFilterHelper
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable, :lockable, validate_on_invite: true
-  extend DateRangeFilterHelper
 
   ADMIN         = 'admin'
   COORDINATOR   = 'coordinator'
