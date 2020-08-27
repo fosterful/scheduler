3bd61c888898be1617e37a3da8174d26048bcd4b
nshoes <nate@hint.io>
2020-08-27 15:43:38 +0000

    use turbolinks load event so toggle links work as expected

--- app/javascript/src/responses.js
+++ app/javascript/src/responses.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  $('#toggle-unavailable-list').on('click', function(e) {
+$(document).on('turbolinks:load', function () {
+  $('#toggle-unavailable-list').on('click', function (e) {
     e.preventDefault();
     $('#unavailable-list').toggleClass('hide');
   });
 
-  $('#toggle-pending-list').on('click', function(e) {
+  $('#toggle-pending-list').on('click', function (e) {
     e.preventDefault();
     $('#pending-list').toggleClass('hide');
   });
