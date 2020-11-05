8453d68597290462eab93d5d618a6b4592cadaf4
cidsinga <cidsinga@gmail.com>
2020-11-05 13:41:06 -0800

    add user mailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notification_email" do
+    let(:email) { 'admin@example.com' }
+    let(:message) { "Its a message." }
+    
+
+    it "send the email, then test that it got queued" do
+      UserMailer.with(email: email, message: message).notification_email.deliver
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    it "Test the body of the sent email contains what we expect it to" do
+      mail = UserMailer.with(email: email, message: message).notification_email
+      expect(mail.body).to include(message)
+    end
+  end
 end
