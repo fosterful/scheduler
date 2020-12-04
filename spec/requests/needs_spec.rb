36aa2e0abe4fdda1dd8878cf21c7c434c608fd2f
Jason Dinsmore <jason@dinjas.com>
2020-12-04 13:38:19 -0800

    Add missing colons

--- spec/requests/needs_spec.rb
+++ spec/requests/needs_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe 'Needs', type: :request do
         expect(Services::TextMessageEnqueue)
           .to receive(:send_messages).once.with(Array, String)
 
-        put need_path(need), params: { need: { children_count 20 } }
+        put need_path(need), params: { need: { children_count: 20 } }
 
         expect(response).to redirect_to(assigns(:need))
       end
@@ -107,7 +107,7 @@ RSpec.describe 'Needs', type: :request do
       it 'renders the edit view' do
         expect_any_instance_of(Need).to receive(:save).and_return(false)
 
-        put need_path(need), params: { need: { children_count 20 } }
+        put need_path(need), params: { need: { children_count: 20 } }
 
         expect(response).to render_template(:edit)
       end
