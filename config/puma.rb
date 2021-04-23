78d193a812cf77b78d5f3526d69053b1c5bfad65
Joel Hayhurst <joelmichael@gmail.com>
2021-04-23 11:32:01 -0700

    Restore on_worker_boot block to puma
    So far as I can tell it is still recommended.

--- config/puma.rb
+++ config/puma.rb
@@ -39,5 +39,9 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
 preload_app!
 
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
