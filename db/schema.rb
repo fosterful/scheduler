87ea8abf1bb057c4b0984e92b2df2487e5ee70a6
Joel Hayhurst <joelmichael@gmail.com>
2021-04-23 10:47:49 -0700

    Run new migrations from app:update
    I had to alter and rollback the migration from active_storage:install,
    lest it conflict with the active_storage:update migrations by trying to
    add the active_storage_variant_records table twice.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_173903) do
+ActiveRecord::Schema.define(version: 2021_04_23_174146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,12 +35,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_173903) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "addresses", force: :cascade do |t|
@@ -220,7 +214,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_173903) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "announcements", "users", column: "author_id"
   add_foreign_key "blockouts", "blockouts", column: "parent_id"
   add_foreign_key "blockouts", "users"
