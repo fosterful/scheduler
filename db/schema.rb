0a0db2d490bba9c02831172d4c3f3c57a8101833
Joel Hayhurst <joelmichael@gmail.com>
2021-04-23 10:51:57 -0700

    Fix active_storage_variant_records migration
    Apparently I didn't include this in the last commit properly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_174146) do
+ActiveRecord::Schema.define(version: 2021_04_23_175037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2021_04_23_174146) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "addresses", force: :cascade do |t|
@@ -214,6 +220,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_174146) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "announcements", "users", column: "author_id"
   add_foreign_key "blockouts", "blockouts", column: "parent_id"
   add_foreign_key "blockouts", "users"
