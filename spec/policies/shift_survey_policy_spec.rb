ca13491915ecff17762f858656a7911b1b6082a8
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-11-08 19:31:46 +0000

    Update policy spec to support new attributes.
    Co-authored-by: Kristen Hopper <krishops@users.noreply.github.com>

--- spec/policies/shift_survey_policy_spec.rb
+++ spec/policies/shift_survey_policy_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe ShiftSurveyPolicy, type: :policy do
     it 'permitted_attributes' do
       result = subject.permitted_attributes
 
-      expect(result).to eql([:notes, :shift_id, :survey_id, :status])
+      expect(result).to eql([:notes, :shift_id, :survey_id, :status, :supplies, :response_time, :hours_match, :ratings, :ratings_text, :comments, :questions])
     end
   end
 
