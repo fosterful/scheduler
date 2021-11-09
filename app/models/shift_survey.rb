3b33f58f5489ae39ee51bedf9c8d24c31dd27ec0
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-11-09 19:44:30 +0000

    Add specs for shift survey worker

--- app/models/shift_survey.rb
+++ app/models/shift_survey.rb
@@ -3,6 +3,10 @@ class ShiftSurvey < ApplicationRecord
   after_create :generate_token
   serialize :ratings, Array
 
+  def to_param
+    token
+  end
+
   def generate_token
     self.update(token: SecureRandom.urlsafe_base64(16))
   end
--- spec/workers/exit_worker_spec.rb
+++ spec/workers/exit_worker_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 RSpec.describe ShiftSurveyWorker do
 
+  describe '#perform' do
+    subject { -> { described_class.new.perform } }
+
+    it { is_expected.not_to change(SendTextMessageWorker.jobs, :size) }
+
+    context 'when there are completed shifts' do
+      let(:office) { create :office }
+      let(:user) { create :user, role: 'coordinator', offices: [office] }
+      let(:volunteer) { create :user, role: 'volunteer', offices: [office] }
+      let(:need) { create :need_with_assigned_shifts, user: user, office: office }
+      let(:shift) { need.shifts.first }
+      let(:shift_survey) { create :shift_survey, shift: shift }
+
+      before do
+        create(:shift, need: need, start_at: 3.hours.ago,
+          created_at: 4.hours.ago, user: volunteer)
+      end
+      
+      it { is_expected.to change(SendTextMessageWorker.jobs, :size) }
+      
+    end
+  end
+
 end
+
