db607a451cb107770beacfb32b1d1e7ec4160dfc
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-11-10 04:31:09 +0000

    Add spec

--- spec/models/shift_survey_spec.rb
+++ spec/models/shift_survey_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ShiftSurvey, type: :model do
   let(:shift_survey) { create :shift_survey }
+  let(:user) { create :user }
   
   it 'has a valid factory' do
     expect(shift_survey.valid?).to be(true)
@@ -17,6 +18,14 @@ RSpec.describe ShiftSurvey, type: :model do
   describe 'to_param' do
     it 'generates a link based on the token' do
       expect(shift_survey.to_param).to eq(shift_survey.token)
+    end
+  end
+
+  describe 'survey_responses_by_shift' do
+    it 'returns a hash of responses by shift' do
+      ShiftSurvey.survey_responses_by_shift(user, Date.yesterday.to_s, Date.tomorrow.to_s).each do |shift, responses|
+        expect(responses.count).to eq(shift.survey_responses.count)
+      end
     end
   end
 
