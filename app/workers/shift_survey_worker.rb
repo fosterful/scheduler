4be9993e348fb8696d4d6e7e4cf6057f44de9e90
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-11-17 00:26:28 +0000

    Update to use references

--- app/workers/shift_survey_worker.rb
+++ app/workers/shift_survey_worker.rb
@@ -22,9 +22,8 @@ class ShiftSurveyWorker
       user_need_shifts = shift.user.shifts.where(need_id: shift.need_id)
       remaining_user_need_shifts = user_need_shifts.where('start_at > ?', shift.start_at).where('need_id = ?', shift.need_id)
       # Only send if there are no remaining_user_need_shifts and the shift has been over for at least 30 minutes
-      if !remaining_user_need_shifts.any? && Time.now > shift.end_at + 30.minutes && shift.shift_survey_id == nil #check for existing survey
+      if !remaining_user_need_shifts.any? && Time.now > shift.end_at + 30.minutes && !shift.shift_survey.present? #check for existing survey
         survey = ShiftSurvey.create!(shift_id: shift.id)
-        user_need_shifts.update_all(shift_survey_id: survey.id)
         message = "Thanks for taking a shift at the child welfare office today! We’d love to know how you’re feeling about it, or if there’s anything that needs our attention. Please take this one-minute survey. #{ shift_survey_path(survey) }"
         send_message([shift.user.phone], message)
       end
