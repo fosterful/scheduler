b245e6f733c93578876c1b4e21daa53e4c50806f
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-11-19 15:12:13 -0800

    Remove sneaky migration that found its way in.

--- db/migrate/20211110172116_add_preferred_language_override_to_need.rb
+++
@@ -1,5 +0,0 @@
-class AddPreferredLanguageOverrideToNeed < ActiveRecord::Migration[6.1]
-  def change
-    add_column :needs, :preferred_language_override, :boolean, default: false, null: false
-  end
-end
