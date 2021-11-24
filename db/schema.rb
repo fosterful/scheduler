2784fd21d4074e66ef378336de23f7699b8d15a4
Michael Reiersgaard <84690831+mireie@users.noreply.github.com>
2021-11-24 15:11:30 -0800

    Update schema version.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_182613) do
+ActiveRecord::Schema.define(version: 2021_11_10_172116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
