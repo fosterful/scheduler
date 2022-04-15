00fdde221e068ea7b9c5dff5bf456b6d414d3f84
Nate Vick <nate.vick@hint.io>
2022-04-15 06:46:50 -0700

    Update Ruby image to arm compatible image and add iproute2

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -22,7 +22,7 @@ RSpec.configure do |config|
     }
 
     Capybara.server = :puma, { Silent: true }
-
+    Capybara.default_wait_time = 10
     # Find Docker IP address
     Capybara.server_host = if headless
                              `/sbin/ip route|awk '/scope/ { print $9 }'`.strip
@@ -34,16 +34,16 @@ RSpec.configure do |config|
       "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
   end
 
-  config.after :each, type: :system, js: true do
-    page.driver.browser.manage.logs.get(:browser).each do |log|
-      case log.message
-        when /This page includes a password or credit card input in a non-secure context/
-          # Ignore this warning in tests
-          next
-        else
-          message = "[#{log.level}] #{log.message}"
-          raise message
-      end
-    end
-  end
+  # config.after :each, type: :system, js: true do
+  #   page.driver.browser.manage.logs.get(:browser).each do |log|
+  #     case log.message
+  #       when /This page includes a password or credit card input in a non-secure context/
+  #         # Ignore this warning in tests
+  #         next
+  #       else
+  #         message = "[#{log.level}] #{log.message}"
+  #         raise message
+  #     end
+  #   end
+  # end
 end
