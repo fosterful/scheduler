579eb2de9f71611939c77591c40b6f3f725c08d4
Oscar M Romero <creaturenex@gmail.com>
2022-07-08 14:02:46 +0000

    Updated form to remove grey background, due to bootstrap theme. Added buttons to expand/close tables from view

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -1,11 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "hideable" ]
+  static targets = [ "hideableHours", "hideableNeeds" ]
 
-  toggleTargets() {
-    this.hideableTargets.forEach((el) => {
+  toggleHoursTargets() {
+    this.hideableHoursTargets.forEach((el) => {
       el.hidden = !el.hidden
+    });
+  }
+
+  toggleNeedsTargets() {
+    this.hideableNeedsTargets.forEach((el) => {
+      el.hidden = !el.hidden
+    });
+  }
+
+  hideHoursTargets() {
+    this.hideableHoursTargets.forEach((el) => {
+      el.hidden = true
+    });
+  }
+
+  hideNeedsTargets() {
+    this.hideableNeedsTargets.forEach((el) => {
+      el.hidden = true
     });
   }
 }
