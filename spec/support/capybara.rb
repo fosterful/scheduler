b06f7acb5e7e4cf72d37a2c7e35edcc0836b9dc6
Benjamin Wood <benjaminwood@gmail.com>
2023-03-17 18:21:14 +0000

    Upgrade Ruby 2.7.6 -> 3.0.5
    Co-authored-by: Nate Vick <nate.vick@hint.io>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -16,9 +16,8 @@ RSpec.configure do |config|
           end
 
     driven_by :selenium, using: :chrome, options: {
-      browser:              :remote,
-      url:                  url,
-      desired_capabilities: :chrome
+      browser: :remote,
+      url:     url
     }
 
     Capybara.server = :puma, { Silent: true }
@@ -34,16 +33,16 @@ RSpec.configure do |config|
       "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
   end
 
-  config.after :each, type: :system, js: true do
-    page.driver.browser.manage.logs.get(:browser).each do |log|
-      case log.message
-        when /This page includes a password or credit card input in a non-secure context/
-          # Ignore this warning in tests
-          next
-        else
-          message = "[#{log.level}] #{log.message}"
-          raise message
-      end
-    end
-  end
+  # config.after :each, type: :system, js: true do
+  #   page.driver.browser.manage.logs.get(:browser).each do |log|
+  #     case log.message
+  #       when /This page includes a password or credit card input in a non-secure context/
+  #         # Ignore this warning in tests
+  #         next
+  #       else
+  #         message = "[#{log.level}] #{log.message}"
+  #         raise message
+  #     end
+  #   end
+  # end
 end
