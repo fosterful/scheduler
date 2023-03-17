f29d9eafe7e1115315093c190b0b69150dde12c0
Nate Vick <nate.vick@hint.io>
2023-03-17 16:44:30 -0700

    Only set Capybara server host if not headless

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -22,12 +22,10 @@ RSpec.configure do |config|
 
     Capybara.server = :puma, { Silent: true }
 
-    # Find Docker IP address
-    Capybara.server_host = if headless
-                             `/sbin/ip route|awk '/scope/ { print $9 }'`.strip
-                           else
-                             '0.0.0.0'
-                           end
+    if !headless
+      Capybara.server_host = "0.0.0.0"
+    end
+    
     Capybara.server_port = '43447'
     Capybara.app_host =
       "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
